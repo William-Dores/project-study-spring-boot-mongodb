e1844c59541e4dd2b427b80e867779f0a747c90f
William-Dores <williamdores.s132@outlook.com>
2024-01-04 01:15:47 -0300

    Getting a User per ID

--- src/main/java/com/estudo/workshopmongodb/repository/UserRepository.java
+++ src/main/java/com/estudo/workshopmongodb/repository/UserRepository.java
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.estudo.workshopmongodb.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String>{
+
+
 
 }
--- src/main/java/com/estudo/workshopmongodb/services/UserService.java
+++ src/main/java/com/estudo/workshopmongodb/services/UserService.java
@@ -1,20 +1,33 @@
 package com.estudo.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudo.workshopmongodb.domain.User;
 import com.estudo.workshopmongodb.repository.UserRepository;
+import com.estudo.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 		@Autowired
 		private UserRepository repo;
-	
+		
 		public List<User> findAll(){
 			return repo.findAll();
 		}
+		
+		public User findById(String id){
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+			
+//			User user = repo.findOne(id);
+//			if (user == null) {
+//				throw new ObjectNotFoundException("Object not found!");
+//			}
+//			return user;
+		}	
 }
