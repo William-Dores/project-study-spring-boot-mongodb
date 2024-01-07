7a23ec55b3098f933c5fb52514015f35376257c5
William-Dores <williamdores.s132@outlook.com>
2024-01-07 03:43:53 -0300

    Deletion of user with method DELETE && Update of user with PUT

--- src/main/java/com/estudo/workshopmongodb/WorkshopmongodbApplication.java
+++ src/main/java/com/estudo/workshopmongodb/WorkshopmongodbApplication.java
@@ -1,7 +1,5 @@
 package com.estudo.workshopmongodb;
 
-import java.util.Iterator;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
