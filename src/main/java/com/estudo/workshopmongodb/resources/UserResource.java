ae1c08ca6e8aa3839e1f7dba006212534719dd35
William-Dores <williamdores.s132@outlook.com>
2024-01-20 00:10:00 -0300

    Endpoint for return the Post of a User

--- src/main/java/com/estudo/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/estudo/workshopmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.estudo.workshopmongodb.domain.Post;
 import com.estudo.workshopmongodb.domain.User;
 import com.estudo.workshopmongodb.dto.UserDTO;
 import com.estudo.workshopmongodb.services.UserService;
@@ -61,5 +62,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 
 	}
+	
+	@RequestMapping(value = "/{id}/post", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 
 }
